10f41b5bf14d4a6c8313d8a42629dfa3b8f76c30
noperezoso <yonoperezoso@protonmail.com>
2025-04-19 03:54:44 +0300

    build: add build scripts

---
+++ build_tools/build.cmd
@@ -0,0 +1,3 @@
+pyinstaller --onefile --name zapretGUI.exe --noconsole --uac-admin main.py
+rem pyinstaller --onedir --name zapretGUI --noconsole --uac-admin main.py
+ 
---
+++ build_tools/generate_ui.cmd
@@ -0,0 +1,2 @@
+pyside6-uic.exe ui\zapretGUI.ui -o zapretGUI.py
+pyside6-uic.exe ui\filters.ui -o filters.py
