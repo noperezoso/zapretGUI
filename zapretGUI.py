0dd525a36a3eee71f1d42bdd77b591db70a7fa3d
noperezoso <yonoperezoso@protonmail.com>
2025-04-19 07:54:39 +0300

    feat: add generated zapretGUI window

---
+++ zapretGUI.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'zapretGUI.ui'
+##
+## Created by: Qt User Interface Compiler version 6.9.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QGroupBox, QMainWindow, QPushButton,
+    QSizePolicy, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(280, 260)
+        MainWindow.setLocale(QLocale(QLocale.Russian, QLocale.Russia))
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.groupBox_1 = QGroupBox(self.centralwidget)
+        self.groupBox_1.setObjectName(u"groupBox_1")
+        self.groupBox_1.setGeometry(QRect(20, 80, 110, 150))
+        self.groupBox_1.setLocale(QLocale(QLocale.Russian, QLocale.Russia))
+        self.groupBox_1.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.pushButton_13 = QPushButton(self.groupBox_1)
+        self.pushButton_13.setObjectName(u"pushButton_13")
+        self.pushButton_13.setGeometry(QRect(15, 85, 80, 20))
+        self.pushButton_13.setLocale(QLocale(QLocale.Russian, QLocale.Russia))
+        self.pushButton_14 = QPushButton(self.groupBox_1)
+        self.pushButton_14.setObjectName(u"pushButton_14")
+        self.pushButton_14.setGeometry(QRect(15, 115, 80, 20))
+        self.pushButton_14.setLocale(QLocale(QLocale.Russian, QLocale.Russia))
+        self.pushButton_11 = QPushButton(self.groupBox_1)
+        self.pushButton_11.setObjectName(u"pushButton_11")
+        self.pushButton_11.setGeometry(QRect(15, 25, 80, 20))
+        self.pushButton_11.setLocale(QLocale(QLocale.Russian, QLocale.Russia))
+        self.pushButton_12 = QPushButton(self.groupBox_1)
+        self.pushButton_12.setObjectName(u"pushButton_12")
+        self.pushButton_12.setGeometry(QRect(15, 55, 80, 20))
+        self.pushButton_12.setLocale(QLocale(QLocale.Russian, QLocale.Russia))
+        self.pushButton_02 = QPushButton(self.centralwidget)
+        self.pushButton_02.setObjectName(u"pushButton_02")
+        self.pushButton_02.setGeometry(QRect(165, 20, 80, 50))
+        self.pushButton_02.setLocale(QLocale(QLocale.Russian, QLocale.Russia))
+        self.groupBox_2 = QGroupBox(self.centralwidget)
+        self.groupBox_2.setObjectName(u"groupBox_2")
+        self.groupBox_2.setGeometry(QRect(150, 80, 110, 150))
+        self.groupBox_2.setLocale(QLocale(QLocale.Russian, QLocale.Russia))
+        self.groupBox_2.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.pushButton_23 = QPushButton(self.groupBox_2)
+        self.pushButton_23.setObjectName(u"pushButton_23")
+        self.pushButton_23.setGeometry(QRect(15, 85, 80, 20))
+        self.pushButton_23.setLocale(QLocale(QLocale.Russian, QLocale.Russia))
+        self.pushButton_24 = QPushButton(self.groupBox_2)
+        self.pushButton_24.setObjectName(u"pushButton_24")
+        self.pushButton_24.setGeometry(QRect(15, 115, 80, 20))
+        self.pushButton_24.setLocale(QLocale(QLocale.Russian, QLocale.Russia))
+        self.pushButton_21 = QPushButton(self.groupBox_2)
+        self.pushButton_21.setObjectName(u"pushButton_21")
+        self.pushButton_21.setGeometry(QRect(15, 25, 80, 20))
+        self.pushButton_21.setLocale(QLocale(QLocale.Russian, QLocale.Russia))
+        self.pushButton_22 = QPushButton(self.groupBox_2)
+        self.pushButton_22.setObjectName(u"pushButton_22")
+        self.pushButton_22.setGeometry(QRect(15, 55, 80, 20))
+        self.pushButton_22.setLocale(QLocale(QLocale.Russian, QLocale.Russia))
+        self.pushButton_00 = QPushButton(self.centralwidget)
+        self.pushButton_00.setObjectName(u"pushButton_00")
+        self.pushButton_00.setGeometry(QRect(35, 20, 80, 50))
+        self.pushButton_00.setLocale(QLocale(QLocale.Russian, QLocale.Russia))
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"zapretGUI", None))
+        self.groupBox_1.setTitle(QCoreApplication.translate("MainWindow", u"\u0421\u043b\u0443\u0436\u0431\u0430", None))
+        self.pushButton_13.setText(QCoreApplication.translate("MainWindow", u"\u0417\u0430\u043f\u0443\u0441\u0442\u0438\u0442\u044c", None))
+        self.pushButton_14.setText(QCoreApplication.translate("MainWindow", u"\u041e\u0441\u0442\u0430\u043d\u043e\u0432\u0438\u0442\u044c", None))
+        self.pushButton_11.setText(QCoreApplication.translate("MainWindow", u"\u0421\u043e\u0437\u0434\u0430\u0442\u044c", None))
+        self.pushButton_12.setText(QCoreApplication.translate("MainWindow", u"\u0423\u0434\u0430\u043b\u0438\u0442\u044c", None))
+        self.pushButton_02.setText(QCoreApplication.translate("MainWindow", u"\u0424\u0438\u043b\u044c\u0442\u0440\u044b", None))
+        self.groupBox_2.setTitle(QCoreApplication.translate("MainWindow", u"\u0417\u0430\u0434\u0430\u0447\u0430", None))
+        self.pushButton_23.setText(QCoreApplication.translate("MainWindow", u"\u0417\u0430\u043f\u0443\u0441\u0442\u0438\u0442\u044c", None))
+        self.pushButton_24.setText(QCoreApplication.translate("MainWindow", u"\u041e\u0441\u0442\u0430\u043d\u043e\u0432\u0438\u0442\u044c", None))
+        self.pushButton_21.setText(QCoreApplication.translate("MainWindow", u"\u0421\u043e\u0437\u0434\u0430\u0442\u044c", None))
+        self.pushButton_22.setText(QCoreApplication.translate("MainWindow", u"\u0423\u0434\u0430\u043b\u0438\u0442\u044c", None))
+        self.pushButton_00.setText(QCoreApplication.translate("MainWindow", u"\u0423\u0441\u0442\u0430\u043d\u043e\u0432\u0438\u0442\u044c", None))
+    # retranslateUi
+
